85c5ad40f75144789accb2847ab4da9d51cd01af
OGFishies <133749513+OgFishies2@users.noreply.github.com>
2023-05-19 10:20:28 +0100

    Update game.py

--- game/game.py
+++ game/game.py
@@ -1,5 +1,12 @@
 print('this is cat ball')
 
-name = input("what is your name")
+name = input("what is your name ")
 print("hi "+name)
+q = int(input("what is 1+1="))
+
+if(q == 2):
+    print("yes")
+
+if not(q == 2):
+    print("no")
 input()
